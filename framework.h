374cd7349e2370ca624979c8052dad989b0509fa
mochineko <mochinekoo0113@gmail.com>
2026-01-08 23:27:04 +0900

    [add] 画面が出るようになりました

---
+++ framework.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace Screen {
+	inline const char* TITLE = "TypeGame";
+	inline const int WIDTH = 1280;
+	inline const int HEIGHT = 720;
+	inline const int COLORBIT = 32;
+	inline const int FPS = 60;
+	inline const int BACKCOLOR[3] = { 0, 0, 0 };
+}
---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <DxLib.h>
+#include "framework.h"
+
+int initApplication();
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+    if (initApplication() != 0) {
+        return -1;
+    }
+
+    while (true) {
+        ClearDrawScreen();
+
+        ScreenFlip();
+        WaitTimer(1000 / Screen::FPS);
+        if (ProcessMessage() == -1) {
+            break;
+        }
+    }
+}
+
+int initApplication() {
+    SetMainWindowText(Screen::TITLE);
+    ChangeFontType(DX_FONTTYPE_ANTIALIASING_EDGE_16X16); //アンチエイリアシング＆エッジが使えるように
+    ChangeWindowMode(true);
+    SetGraphMode(Screen::WIDTH, Screen::HEIGHT, Screen::COLORBIT);
+    if (DxLib_Init() == -1) {
+        return -1;
+    }
+
+    SetBackgroundColor(Screen::BACKCOLOR[0], Screen::BACKCOLOR[1], Screen::BACKCOLOR[2]);
+    SetDrawScreen(DX_SCREEN_BACK);
+
+    return 0;
+}
