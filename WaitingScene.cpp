2f00c735775f4e782f545f669ca8e87d6a3a9979
mochineko <mochinekoo0113@gmail.com>
2026-01-08 23:52:39 +0900

    [add] 文字を追加しました

--- WaitingScene.cpp
+++ WaitingScene.cpp
@@ -1,6 +1,8 @@
 #include "WaitingScene.h"
 #include <DxLib.h>
 #include <assert.h>
+#include "GameUtility.h"
+#include "framework.h"
 
 WaitingScene::WaitingScene()
 {
@@ -22,4 +24,6 @@ void WaitingScene::Draw() {
 	if (backgroundImageHandle != -1) {
 		DrawGraph(0, 0, backgroundImageHandle, TRUE);
 	}
+	GameUtility::DrawFix2DText(CENTER, Screen::WIDTH / 2, (Screen::HEIGHT / 2)-30*1, 30, "タイピングゲーム", Color::WHITE, Color::BLACK);
+	GameUtility::DrawFix2DText(CENTER, Screen::WIDTH / 2, Screen::HEIGHT / 2, 30, "～スペースキーを押してください～", Color::WHITE, Color::BLACK);
 }
